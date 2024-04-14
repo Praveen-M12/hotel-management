a48de74b9fd43a8e401be9af953ea82756a3e1c6
DESKTOP-GHR1TMQ\prave <praveenm@bicsinfotech.com>
2024-04-14 10:01:32 +0530

    change mongoURL

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// const mongoURL = process.env.MONGODB_URL;
+const mongoURL = process.env.MONGODB_URL;
 // const mongoURL = process.env.LOCAL_URL;
-const mongoURL = 'mongodb://localhost:27017/hotels';
+// const mongoURL = 'mongodb://localhost:27017/hotels';
 
 mongoose.connect(mongoURL, { 
     useNewUrlParser: true, 
