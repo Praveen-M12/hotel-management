f4b1dd375f5f68f659f4b7ab5fc1b8a9556bd3cc
DESKTOP-GHR1TMQ\prave <praveenm@bicsinfotech.com>
2024-04-14 09:51:18 +0530

    new add

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,48 +1,108 @@
 const express = require('express');
 const router = express.Router();
 const Person = require('../models/person');
+const {jwtAuthMiddleware, generateToken} = require('../jwt');
 
-router.post('/', async (req, res) => {
-    try {
-        const data= req.body;
-        const newPerson=new Person(data);
+// POST route to add a person
+router.post('/signup', async (req, res) =>{
+    try{
+        const data = req.body // Assuming the request body contains the person data
 
+        // Create a new Person document using the Mongoose model
+        const newPerson = new Person(data);
+
+        // Save the new person to the database
         const response = await newPerson.save();
-        console.log("data saved");
-        res.status(200).json(response);
-    } 
-    catch (error) {
-        console.log(error);
-        res.status(500).json({error:"internal server error"});
-    }
-});
+        console.log('data saved');
 
-router.get('/', async (req, res) => {
-    try {
-        const users = await Person.find();
-        console.log("Data fetched");
-        res.status(200).json(users)
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error:"internal server error"});
-    }
-});
-
-router.get('/:workType', async (req, res) => {
-    try {
-        const workType = req.params.workType; // Extract the work type from the URL parameter
-        if (workType == 'chef' || workType == 'manager' || workType == 'waiter') {
-            const response = await Person.find({ work: workType });
-            console.log('response fetched');
-            res.status(200).json(response);
-        } else {
-            res.status(404).json({ error: 'Invalid work type' });
+        const payload = {
+            id: response.id,
+            username: response.username
         }
-    } catch (err) {
+        console.log(JSON.stringify(payload));
+        const token = generateToken(payload);
+        console.log("Token is : ", token);
+
+        res.status(200).json({response: response, token: token});
+    }
+    catch(err){
         console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
+
+// Login Route
+router.post('/login', async(req, res) => {
+    try{
+        // Extract username and password from request body
+        const {username, password} = req.body;
+
+        // Find the user by username
+        const user = await Person.findOne({username: username});
+
+        // If user does not exist or password does not match, return error
+        if( !user || !(await user.comparePassword(password))){
+            return res.status(401).json({error: 'Invalid username or password'});
+        }
+
+        // generate Token 
+        const payload = {
+            id: user.id,
+            username: user.username
+        }
+        const token = generateToken(payload);
+
+        // resturn token as response
+        res.status(200).json({message:"User login Successfully",token:token})
+    }catch(err){
+        console.error(err);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+// Profile route
+router.get('/profile', jwtAuthMiddleware, async (req, res) => {
+    try{
+        const userData = req.user;
+        console.log("User Data: ", userData);
+
+        const userId = userData.id;
+        const user = await Person.findById(userId);
+
+        res.status(200).json({user});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+// GET method to get the person
+router.get('/', jwtAuthMiddleware, async (req, res) =>{
+    try{
+        const data = await Person.find();
+        console.log('data fetched');
+        res.status(200).json(data);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
+
+router.get('/:workType', async(req, res)=>{
+    try{
+        const workType = req.params.workType; // // Extract the work type from the URL parameter
+        if(workType == 'chef' || workType == 'manager' || workType == 'waiter' ){
+            const response = await Person.find({work: workType});
+            console.log('response fetched');
+            res.status(200).json(response);
+        }else{
+            res.status(404).json({error: 'Invalid work type'});
+        }
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
 
 router.put('/:id', async (req, res)=>{
     try{
